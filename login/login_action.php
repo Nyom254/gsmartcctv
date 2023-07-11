599ff606734050d54237457ace877d82f2d8c733
yovan giofrey <yovangiofrey7@gmail.com>
2023-07-11 11:52:14 +0700

    mengubah cara pengolahan password

--- login/login_action.php
+++ login/login_action.php
@@ -3,27 +3,30 @@ include "../conn.php";
 session_start();
 
 
-$queryUser = mysqli_prepare($conn, "select * from user where username = ? and password = ? and status_aktif = 1");
-mysqli_stmt_bind_param($queryUser, "ss", $username, $password);
+$queryUser = mysqli_prepare($conn, "select * from user where username = ? and status_aktif = 1");
+mysqli_stmt_bind_param($queryUser, "s", $username);
 
 $username = mysqli_escape_string($conn, $_POST['username']);
-$password = mysqli_escape_string($conn, $_POST['password']);
-
+$password = $_POST['password'];
 mysqli_stmt_execute($queryUser);
 
 $dataUser = mysqli_stmt_get_result($queryUser);
 $cekDataUser = $dataUser->num_rows;
+$rowUser = mysqli_fetch_assoc($dataUser);
 
 if ($cekDataUser > 0) {
-    $rowUser = mysqli_fetch_assoc($dataUser);
-    $_SESSION['username'] = $username;
-    $_SESSION['id_user'] = $rowUser['id_user'];
-    if ($rowUser['level'] == '1') {
-        $_SESSION['status'] = "master_login";
-        header("location:../home/index.php");
-    } else if ($rowUser['level'] == '0') {
-        $_SESSION['status'] = "user_login";
-        header("location:../home/index.php");
+    if (password_verify($password, $rowUser['password'])) {
+        $_SESSION['username'] = $username;
+        $_SESSION['id_user'] = $rowUser['id_user'];
+        if ($rowUser['level'] == '1') {
+            $_SESSION['status'] = "master_login";
+            header("location:../home/index.php");
+        } else if ($rowUser['level'] == '0') {
+            $_SESSION['status'] = "user_login";
+            header("location:../home/index.php");
+        }
+    } else {
+        header("location:./login_page.php?m=gagal");
     }
 } else {
     header("location:./login_page.php?m=gagal");
