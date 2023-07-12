75e766e2f79909f498db299d001ad38fc371256c
yovan giofrey <yovangiofrey7@gmail.com>
2023-07-12 15:21:44 +0700

    menghapus kode yang tidak perlu

--- home/inventaris_kantor/inventaris_kantor.php
+++ home/inventaris_kantor/inventaris_kantor.php
@@ -105,7 +105,6 @@
                                 </thead>
                                 <tbody>
                                     <?php
-                                    mysqli_query($conn, "SET SESSION TRANSACTION ISOLATION LEVEL READ UNCOMMITTED");
                                     $queryInventarisKantor = mysqli_query($conn, "select no_inventaris, tgl, nama_barang, posisi, keterangan, qty, attachment from inventaris_kantor");
                                     if ($queryInventarisKantor->num_rows > 0) {
                                         while ($rowInvetarisKantor = mysqli_fetch_assoc($queryInventarisKantor)) { ?>
@@ -230,7 +229,6 @@
                                             <!-- /.modal -->
                                     <?php
                                         }
-                                        mysqli_query($conn, "SET SESSION TRANSACTION ISOLATION LEVEL REPEATABLE READ ;");
                                     }
 
 
