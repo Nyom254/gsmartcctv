88cf2a7eb08bfdc71b7efa7df54915ddbce349c9
yovan giofrey <yovangiofrey7@gmail.com>
2023-07-27 08:49:02 +0700

    try fix cannot modify header information

--- home/penjualan/pembayaran_customer/tambah_pembayaran_customer_action.php
+++ home/penjualan/pembayaran_customer/tambah_pembayaran_customer_action.php
@@ -9,40 +9,41 @@ require_once("../../../tinify-php-master/lib/Tinify/Client.php");
 require_once("../../../tinify-php-master/lib/Tinify.php");
 include '../../../conn.php';
 
-$queryApi = mysqli_query($conn, "select * from `api_key` where name = 'tinify'");
-$dataApi = mysqli_fetch_assoc($queryApi);
-$keyTinyApi = $dataApi['key'];
-\Tinify\setKey("$keyTinyApi");
-
-$queryTambahPembayaranCustomer = mysqli_prepare($conn, "INSERT INTO pembayaran_customer (no_pembayaran, tanggal, kode_departemen, kode_customer, jenis_pembayaran, nama_bank, attachment, total, selisih, cruser) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-mysqli_stmt_bind_param($queryTambahPembayaranCustomer, "sssssssdds", $no_pembayaran, $tanggal, $kode_departemen, $kode_customer, $jenis_pembayaran, $nama_bank, $attachment, $total, $selisih, $cruser);
-
-$no_pembayaran = mysqli_escape_string($conn, $_POST['no_pembayaran']);
-$tanggal = mysqli_escape_string($conn, $_POST['tanggal']);
-$kode_departemen = mysqli_escape_string($conn, $_POST['departemen']);
-$kode_customer = mysqli_escape_string($conn, $_POST['customer']);
-$jenis_pembayaran = mysqli_escape_string($conn, $_POST['jenis_pembayaran']);
-$nama_bank = mysqli_escape_string($conn, $_POST['nama_bank']);
-$total = mysqli_escape_string($conn, $_POST['totalBayar']);
-$selisih = mysqli_escape_string($conn, $_POST['selisih']);
-$attachment = null;
-$cruser = $_SESSION['username'];
-
-$no_invoice = $_POST['no-invoice'];
-$tagihan = $_POST['tagihan'];
-$potongan = $_POST['potongan'];
-$bayar = $_POST['bayar'];
-
-
-$queryLogTambahInvenTarisKantor = mysqli_prepare($conn, "insert into log_transaksi (NO_TRANSAKSI, ACTION, KETERANGAN, USERID) values (?, ?, ?, ?)");
-mysqli_stmt_bind_param($queryLogTambahInvenTarisKantor, "ssss", $no_pembayaran, $action, $keteranganLog, $userid);
-
-$action = "INSERT";
-$keteranganLog = $_SESSION['username'] . " menambahkan pembayaran customer  " . $no_pembayaran;
-$userid = $_SESSION['id_user'];
-
 
 if (array_key_exists('no-invoice', $_POST)) {
+
+    $queryApi = mysqli_query($conn, "select * from `api_key` where name = 'tinify'");
+    $dataApi = mysqli_fetch_assoc($queryApi);
+    $keyTinyApi = $dataApi['key'];
+    \Tinify\setKey("$keyTinyApi");
+
+    $queryTambahPembayaranCustomer = mysqli_prepare($conn, "INSERT INTO pembayaran_customer (no_pembayaran, tanggal, kode_departemen, kode_customer, jenis_pembayaran, nama_bank, attachment, total, selisih, cruser) VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+    mysqli_stmt_bind_param($queryTambahPembayaranCustomer, "sssssssdds", $no_pembayaran, $tanggal, $kode_departemen, $kode_customer, $jenis_pembayaran, $nama_bank, $attachment, $total, $selisih, $cruser);
+
+    $no_pembayaran = mysqli_escape_string($conn, $_POST['no_pembayaran']);
+    $tanggal = mysqli_escape_string($conn, $_POST['tanggal']);
+    $kode_departemen = mysqli_escape_string($conn, $_POST['departemen']);
+    $kode_customer = mysqli_escape_string($conn, $_POST['customer']);
+    $jenis_pembayaran = mysqli_escape_string($conn, $_POST['jenis_pembayaran']);
+    $nama_bank = mysqli_escape_string($conn, $_POST['nama_bank']);
+    $total = mysqli_escape_string($conn, $_POST['totalBayar']);
+    $selisih = mysqli_escape_string($conn, $_POST['selisih']);
+    $attachment = null;
+    $cruser = $_SESSION['username'];
+
+    $no_invoice = $_POST['no-invoice'];
+    $tagihan = $_POST['tagihan'];
+    $potongan = $_POST['potongan'];
+    $bayar = $_POST['bayar'];
+
+
+    $queryLogTambahInvenTarisKantor = mysqli_prepare($conn, "insert into log_transaksi (NO_TRANSAKSI, ACTION, KETERANGAN, USERID) values (?, ?, ?, ?)");
+    mysqli_stmt_bind_param($queryLogTambahInvenTarisKantor, "ssss", $no_pembayaran, $action, $keteranganLog, $userid);
+
+    $action = "INSERT";
+    $keteranganLog = $_SESSION['username'] . " menambahkan pembayaran customer  " . $no_pembayaran;
+    $userid = $_SESSION['id_user'];
+
     if (file_exists($_FILES['attachment']['tmp_name']) || is_uploaded_file($_FILES['attachment']['tmp_name'])) {
         if (filesize($_FILES['attachment']['tmp_name']) < 20000000) {
             if ($_FILES['attachment']['error'] === UPLOAD_ERR_OK) {
